d0ea637a8cb435376d7d9f6f5d80cdbb83b740fe
Mariia Akhmedova <marry.bellows@gmail.com>
2021-06-03 03:46:30 -0600

    custom exception

--- SEF/src/sef/module8/activity/CustomExceptionActivity.java
+++ SEF/src/sef/module8/activity/CustomExceptionActivity.java
@@ -9,6 +9,7 @@ public class CustomExceptionActivity extends Exception {
 
     @Override
     public String getMessage() {
+
         return "You are not allowed to use this program";
     }
 }
--- SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
+++ SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
@@ -3,29 +3,47 @@ package sef.module8.activity;
 //This program accepts a user name and checks user's validity
 //The user name is checked against an array of names.
 
-public class CustomExceptionActivityTest {
+public class CustomExceptionActivityTest extends Exception {
+    public static void main(String[] args) {
+        CustomExceptionActivityTest custom = new CustomExceptionActivityTest();
+        try {
+            custom.validateUser("John");
+            custom.validateUser("Sarah");
+        } catch (CustomExceptionActivity e) {
+        }
+    }
 
-	//1 - Type main method and call validateUser() from it.
-	//Call this method with different names to test it 
+
+    //1 - Type main method and call validateUser() from it.
+    //Call this method with different names to test it
 
 
-void validateUser(String name)
-{
-	//array of names
-String []validUsers={"John","Mike","Shanti","Stacie"};
+    void validateUser(String name) throws CustomExceptionActivity {
+        //array of names
+        String[] validUsers = {"John", "Mike", "Shanti", "Stacie"};
 //flag stores 1 if a match is found else it should remain 0 
-int flag=0;
-for(int i=0;i<4;i++)
-{
-	
+        int flag = 0;
+        for (int i = 0; i < 4; i++) {
+            String userToCheck = validUsers[i];
+            if (userToCheck.equals(name)) {
+                flag = 1;
+            }
+
+
 //2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly 
 
-}
+        }
 //3 - check if flag is zero, throw CustomExceptionActivity Exception.
 //You may also have to declare this exception in the method call using throws.
 
+        if (flag == 0) {
+            throw new CustomExceptionActivity();
+        }
+        if (flag == 1) {
+            System.out.println("Welcome to Payroll program");
+        }
 
 //4 - else if flag is one, print a message "Welcome to Payroll program". 
 
-}
+    }
 }
