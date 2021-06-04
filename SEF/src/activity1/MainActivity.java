9ce5788b804cdc5f734cc7566e7db495e594c19a
Mariia Akhmedova <marry.bellows@gmail.com>
2021-06-04 05:40:01 -0600

    Person

---
+++ SEF/src/activity1/MainActivity.java
@@ -0,0 +1,13 @@
+package activity1;
+
+public class MainActivity {
+
+    public static void main(String[] args) {
+
+
+        Person person = new Person("Mary", 23);
+        person.introduce();
+
+
+    }
+}
